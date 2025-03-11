dd1cfc3f14f0e1bc8fcd248ee0126f5430207b65
MushrooM2027 <adityaprakash140103@gmail.com>
2025-03-11 10:42:50 +0530

    new start

---
+++ main.java
@@ -0,0 +1,9 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
